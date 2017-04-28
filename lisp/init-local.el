bac046517afeaa2f62e0b236e6da0932c442a049
krypton <krypton@localhost>
2017-04-28 11:29:41 +0800

    add markdown-mode snippet and init-local.el

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -1,30 +1,44 @@
+;;; init-local.el --- personal settings
 ;; customize my habbit
+;;; Code:
+
 (setq-default blink-cursor-mode t)
 (setq-default menu-bar-mode nil)
 (setq-default auto-save-default nil)
 
+
 ;; org-publish site
-(require 'ox-publish)
+
+;;; Commentary:org-publish setting personal site
+
+;; (require-package 'ox-publish)
 (setq org-html-coding-system 'utf-8-unix)
 
 (
  setq org-publish-project-alist
       '(
-
         ("org-notes"
+         ;; directory settings
          :base-directory "~/org-file/"
          :base-extension "org"
          :publishing-directory "~/public_html/"
          :recursive t
          ;; :exclude ".*-reveal\.org"        ; exclude org-reveal slides
          :publishing-function org-html-publish-to-html
+
+         ;; sitemap settings
          :headline-levels 2               ; Just the default for this project.
          :auto-sitemap t                  ; Generate sitemap.org automagically...
-         :sitemap-filename "sitemap.org"  ; ... call it sitemap.org (it's the default)...
-         :sitemap-title "Sitemap"         ; ... with title 'Sitemap'.
+         :sitemap-filename "index.org"  ; ... call it sitemap.org (it's the default)...
+         :sitemap-title "Master Missy"         ; ... with title 'Sitemap'.
+         :sitemap-sort-files anti-chronologically
+         :sitemap-file-entry-format "%d %t"
          :with-creator nil    ; Disable the inclusion of "Created by Org" in the postamble.
-         :with-email "707624376@qq.com"      ; Disable the inclusion of "(your email)" in the postamble.
+         :with-email nil      ; Disable the inclusion of "(your email)" in the postamble.
          :with-author "Rory"       ; Enable the inclusion of "Author: Your Name" in the postamble.
+         :author "Rory"
+
+         ;; css things
          :auto-preamble t;         ; Enable auto preamble
          :auto-postamble t         ; Enable auto postamble
          :table-of-contents t        ; Set this to "t" if you want a table of contents, set to "nil" disables TOC.
@@ -32,7 +46,7 @@
          :section-numbers nil        ; Set this to "t" if you want headings to have numbers.
          :html-head-include-default-style nil ;Disable the default css style
          :html-head-include-scripts nil ;Disable the default javascript snippet
-         :html-head "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n<link rel=\"stylesheet\" type=\"text/css\" href=\"http://www.i3s.unice.fr/~malapert/css/worg.min.css\"/>\n<script type=\"text/javascript\" src=\"http://www.i3s.unice.fr/~malapert/js/ga.min.js\"></script>" ;Enable custom css style and other tags
+         :html-head "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n<link rel=\"stylesheet\" type=\"text/css\" href=\"css/worg.min.css\"/>\n<script type=\"text/javascript\" src=\"js/ga.min.js\"></script>" ;Enable custom css style and other tags
          :html-link-home "index.html"    ; Just the default for this project.
          :html-link-up "../index.html"    ; Just the default for this project.
          )
@@ -54,4 +68,11 @@
       )
 
 
+;; yas/nippet setting;  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(require-package 'yasnippet)
+(yas-global-mode 1) ;; or M-x yas-reload-all if you've started YASnippet already.
+
 (provide 'init-local)
+
+;;; init-local.el ends here
